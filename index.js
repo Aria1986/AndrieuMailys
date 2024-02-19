98a43c902687306aeac354425eea8ed2ea8c0c1d
maimai <mailys.andrieu.simplon@gmail.com>
2024-02-19 16:17:31 +0100

    parallax ok

---
+++ index.js
@@ -0,0 +1,37 @@
+var montagneDevant = document.getElementById('montagnes_devant');
+var montagneMilieu = document.getElementById('montagnes_milieu');
+var montagneArriere = document.getElementById('montagnes_arriere');
+var rapace = document.getElementById('rapace');
+var paysage = document.getElementById('paysage');
+
+new simpleParallax(montagneDevant,{
+    orientation:'up'
+});
+new simpleParallax(montagneArriere,{
+    orientation:'down right'
+});
+new simpleParallax(montagneMilieu,{
+    orientation:'down'
+});
+
+
+new simpleParallax(rapace,{
+    orientation:'down',
+    scale:1.2
+});
+
+(function() {
+    // Add event listener
+    paysage.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#rapace");
+    // Magic happens here
+    function parallax(e) {
+        const position = elem.getAttribute("value");
+        const x =  e.pageX / window.innerWidth;
+        const y = e.pageY / window.innerHeight;
+    
+        elem.style.transform = `translateX(${-x*10}px) translateY(${-y*10}px)`;
+        console.log(position)
+    }
+
+})();
